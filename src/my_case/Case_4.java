aaf038edb6e8c4f224966112cf7b035a0713de37
Spark Lin <sparklin9092@gmail.com>
2023-03-19 08:20:42 +0800

    Case 4

---
+++ src/my_case/Case_4.java
@@ -0,0 +1,13 @@
+package my_case;
+
+public class Case_4 {
+
+	public static void main(String[] args) {
+		
+		var tempStr = "";
+		System.out.println("tempStr is Blank? Ans: " + tempStr.isBlank());
+		
+		var tempStr2 = "Case 4 ";
+		System.out.println("tempStr2 is Empty? Ans: " + tempStr2.isEmpty());
+	}
+}
